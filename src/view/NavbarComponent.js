499b2789950f9cf358e5de47fdcd6d03132505d2
nosillard <nosillard@IUTCLINFB1014>
2024-03-05 11:59:19 +0100

    Q2 links à revoir

--- src/view/NavbarComponent.js
+++ src/view/NavbarComponent.js
@@ -6,7 +6,7 @@ export default {
 
             const linkService = new LinkService();
 
-            links:[] = linkService.getlinks();
+            const links = linkService.getLinks();
             this.$emit('displayLinks', links);
         }
     },
@@ -17,7 +17,7 @@ export default {
             <h1>Navbar</h1>
 
                     <div v-for = "link in links" :key = "links.id">
-                        <a href={{ link.label }}>lien</a>
+                        <a :href="link.link">{{ link.label }}</a>
                     </div>
         </section>
         `
