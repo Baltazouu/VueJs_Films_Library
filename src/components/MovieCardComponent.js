b576bad93ee4fb3e01f37973c34576a2ff6430b6
Baptiste D <baltazar.14130@gmail.com>
2024-03-15 11:19:28 +0100

    ajout couleur vert si film vu

--- src/components/MovieCardComponent.js
+++ src/components/MovieCardComponent.js
@@ -1,11 +1,23 @@
 export default {
+    data() {
+        return {
+            color: "black"
+        };
+    },
     props: {
         movie: {
             type: Object,
             required: true
+        }
+    },
+    methods: {
+        filmVu() {
+            if (this.color === "black") {
+                this.color = "green";
+            } else {
+                this.color = "black";
             }
         },
-    methods: {
         truncateDescription(description) {
             if (description.length > 20) {
                 return description.substring(0, 20) + '...';
@@ -14,23 +26,15 @@ export default {
             }
         }
     },
-    mounted(){
-        console.log("ICIC")
-    },
-    template:`
-    <div class="movie-card">
-    <img :src="movie.image" :alt="movie.title" class="img-fluid rounded-top" />
-    <div class="card-body">
-      <h2 class="card-title">{{ movie.title }}</h2>
-      <p class="card-text">{{ truncateDescription(movie.description) }}</p>
+    mounted() {},
+    template: `
+    <div class="card h-100">
+      <img :src="movie.image" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" :style="{ color: color }">{{ movie.title }}</h5>
+        <p class="card-text">{{ truncateDescription(movie.description) }}</p>
+        <button @click="filmVu">Film Vu ?</button>
+      </div>
     </div>
-  </div>  
-`
-}
-/*
-<div class="movieCard">
-            <h2>{{movie.title}}</h2>
-            <p>{{truncateDescription(movie.description)}}</p>
-            <img :src="movie.image" :alt="movie.title">        
-        </div>
-*/
+  `
+};
--- src/view/AllMoviesView.js
+++ src/view/AllMoviesView.js
@@ -16,42 +16,13 @@ export default {
     <div class="container">
     <h3>Films à l'affiche</h3>
     <div class="row">
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4" v-for="movie in movies" :key="movie.id">
-        <div class="card h-100">
-          <img :src="movie.image" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">{{ movie.title }}</h5>
-            <p class="card-text">{{ movie.description }}</p>
-          </div>
-        </div>
-      </div>
+    
+    <div class="col-lg-3 col-md-4 col-sm-6 mb-4" v-for="movie in movies" :key="movie.id">
+        <movie-card :movie="movie"></movie-card>
+    </div>
+    <movie-card v-for="movie in movies" :movie="movie"></movie-card>
+
     </div>
   </div>
     `
 };
-/*
-<div>
-            <h3>Films à l'affiche</h3>
-            <div class="allMovies">
-                <div v-for="movie in movies" :key="movie.id">
-                    <movie-card :movie="movie"/>
-                </div>
-            </div>
-        </div>
-*/
-/*
-    <div class="container">
-    <h3>Films à l'affiche</h3>
-    <div class="row">
-      <div class="col-lg-3 col-md-4 col-sm-6 mb-4" v-for="movie in movies" :key="movie.id">
-        <div class="card h-100">
-          <img :src="movie.image" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">{{ movie.title }}</h5>
-            <p class="card-text">{{ movie.description }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-   */
