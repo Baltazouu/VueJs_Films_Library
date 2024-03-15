335e4707e4e86b3c6b89d7bd810beb265faca572
Baptiste D <baltazar.14130@gmail.com>
2024-03-15 15:58:55 +0100

    input working

--- src/view/AllMoviesView.js
+++ src/view/AllMoviesView.js
@@ -4,24 +4,40 @@ import { getMovies } from "../misc/movies.js";
 export default {
     data() {
         return {
-            movies: []
+            movies: [],
+            movieSearch:""
         };
     },
     mounted() {
         const storedMovies = JSON.parse(localStorage.getItem('movies')) || [];
         this.movies = [...storedMovies, ...getMovies()];
-        console.log("Movies: ", this.movies);
+    },
+
+    methods:{
+        moviesContains(text){
+
+            // reset movie list
+            const storedMovies = JSON.parse(localStorage.getItem('movies')) || [];
+            this.movies = [...storedMovies, ...getMovies()];
+
+            if(text.trim() === ''){
+                return;
+            }
+
+            this.movies = this.movies.filter(movie => movie.title.toLowerCase().includes(text.toLowerCase()));
+        }
     },
     template: `
     <div class="container">
     <h3>Films à l'affiche</h3>
+    <label for="search">Rechercher Un Film : </label>
+    <input v-model="movieSearch" @input="moviesContains(movieSearch)" name="search">
     <div class="row">
     
     <div class="col-lg-3 col-md-4 col-sm-6 mb-4" v-for="movie in movies" :key="movie.id">
         <movie-card :movie="movie"></movie-card>
     </div>
-    <movie-card v-for="movie in movies" :movie="movie"></movie-card>
-
+   
     </div>
   </div>
     `
